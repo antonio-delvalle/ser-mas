a9bc8bb38e78ced821990e64c0ae7557a2742823
Antonio del Valle <antonio.delvalle@konfio.mx>
2020-05-14 10:46:26 -0500

    Feature: Cleanup Activities

---
+++ src/components/service-main-content.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Img from "gatsby-image";
+
+const ServiceMainContent = ({ text, image }) => {
+  return (
+    <div className="md:flex">
+      <div className="md:w-1/2">
+        <div
+          className="md:mr-4 mb-4"
+          dangerouslySetInnerHTML={{
+            __html: text.childMarkdownRemark.html,
+          }}
+        />
+      </div>
+      <div className="md:w-1/2">
+        <Img fluid={image.fluid} />
+      </div>
+    </div>
+  );
+};
+
+export default ServiceMainContent;
