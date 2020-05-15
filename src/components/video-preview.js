b8766a4c94b671ed8b94e6264416ff4943341592
Antonio del Valle <antonio.delvalle@konfio.mx>
2020-05-14 21:10:46 -0500

    Bugfix: Fixes Slick

--- src/components/video-preview.js
+++ src/components/video-preview.js
@@ -5,7 +5,7 @@ import Img from "gatsby-image";
 const VideoPreview = ({ image, url }) => {
   return (
     <div className="center-video-preview-container">
-      <a target="_blank" href={url}>
+      <a target="_blank" rel="noopener noreferrer" href={url}>
         <FontAwesomeIcon className="center-video-preview text-12xl border-8 border-white z-10 bg-white rounded-full text-sermas-green-200" icon="play-circle" />
         <Img className="video-preview-image-opacity rounded-lg" fluid={image.fluid} />
       </a>
