1b5f9e82552ef94eb1a741f9b049a6325aececaf
Antonio Del Valle <antonio@antoniodelvalle.net>
2020-05-23 17:22:06 -0500

    Feature: Purging CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: false,
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
     fontFamily: {
       sans: ['"DM Sans"', "sans-serif"],
